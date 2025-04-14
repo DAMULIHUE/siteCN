5267a08097b1002b8d529ab33f1a58d454c3dce2
DAMULIHUE <lihue@outlook.com.br>
2025-04-14 01:15:17 +0100

    adicionando gradiente ao titulo, alterando posicoes e tornando mais reponsivo, alterando enunciados e suas posicoes

--- script.js
+++ script.js
@@ -29,29 +29,36 @@ function escolhaUmDois(opx){
 }
 
 function nextDiv(divAtual){
-    //jeito cringe
-    /*document.getElementById("homepage").style.visibility = "hidden";
-    document.getElementById("homepage").style.display = "none";
-
-    document.getElementById("q1").style.visibility = "visible"
-    document.getElementById("q1").style.display = "flex";*/
 
     if(divAtual == 'homepage'){
         document.getElementById(divAtual).style.visibility = "hidden";
         document.getElementById(divAtual).style.display = "none";
+        document.getElementById('homepageHeader').style.visibility = "hidden";
+        document.getElementById('homepageHeader').style.display = "none";
 
         document.getElementById("q1").style.visibility = "visible"
         document.getElementById("q1").style.display = "flex";
+        document.getElementById("q1Header").style.visibility = "visible"
+        document.getElementById("q1Header").style.display = "flex";
     }else{
-        // transforma em array para podermos retirar o 'q'
-        let qx = divAtual.split("").filter(element => element != 'q');
-        let qNumber = Number(qx);
-        let nextq = `q${++qNumber}`;
+        // transforma em array para podermos retirar o 'q', 
+        // retirando o 'q' e 
+        // transformando em numero kk
+        let qx = Number(divAtual.split("").filter(element => element != 'q'));
+        let nextq = `q${++qx}`;
+        let nextqHeader = `${nextq}Header`;
+        let divAtualHeader = `${divAtual}Header`;
         
+        // escondendo elementos do main e do header
         document.getElementById(divAtual).style.visibility = 'hidden';
         document.getElementById(divAtual).style.display = 'none';
+        document.getElementById(divAtualHeader).style.visibility = 'hidden';
+        document.getElementById(divAtualHeader).style.display = 'none';
 
+        // revelando elementos da proxima divMain e do proximo header
         document.getElementById(nextq).style.visibility = 'visible';
         document.getElementById(nextq).style.display = 'flex';
+        document.getElementById(nextqHeader).style.visibility = 'visible';
+        document.getElementById(nextqHeader).style.display = 'flex';
     }
 }
