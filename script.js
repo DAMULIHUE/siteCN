7c3f70cc6bf339517f337bb0e3ce189c363fab7d
DAMULIHUE <lihue@outlook.com.br>
2025-04-17 13:39:17 +0100

    adicionando getResText() e finalizacao do quiz pela metade

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+let respostas = [];
+let opAtual;
+let apagaInputAtual;
+
+var elements = document.getElementsByTagName("INPUT");
+for (var inp of elements) {
+    if (inp.type === "checkbox" || inp.type === "text"){
+        inp.checked = false;
+        inp.value = "";
+    }
+}
 
 function escolhaUm(opx){
-    let box1 = "op1";
+    /*let box1 = "op1";
     let box2 = "op2";
     let box3 = "op3";
     let box4 = "op4";
@@ -11,21 +22,22 @@ function escolhaUm(opx){
         if(opx != element){
             document.getElementById(element).checked = false;
         }
-    });
+    });*/
+
+    for (var inp of elements) {
+        if (inp.type === "checkbox" || inp.type === "text"){
+            inp.checked = false;
+            inp.value = "";
+        }
+    }
+    document.getElementById(opx).checked = true;
+
+    opAtual = opx;
 }
 
-
-function escolhaUmDois(opx){
-    let boxf = 'f';
-    let boxv = 'v';
-
-    let arrBoxes = [boxf, boxv];
-
-    arrBoxes.forEach(element => {
-        if(opx != element){
-            document.getElementById(element).checked = false;
-        }
-    });
+function getInputText(id) {
+    //pegar com base no id passado como parametro
+    opAtual = document.getElementById(id).value;
 }
 
 function nextDiv(divAtual){
@@ -41,6 +53,8 @@ function nextDiv(divAtual){
         document.getElementById("q1Header").style.visibility = "visible"
         document.getElementById("q1Header").style.display = "flex";
     }else{
+        
+
         // transforma em array para podermos retirar o 'q', 
         // retirando o 'q' e 
         // transformando em numero kk
@@ -60,5 +74,11 @@ function nextDiv(divAtual){
         document.getElementById(nextq).style.display = 'flex';
         document.getElementById(nextqHeader).style.visibility = 'visible';
         document.getElementById(nextqHeader).style.display = 'flex';
+
+        respostas.push(opAtual);
+        console.log(opAtual);
     }
+
+    
 }
+
